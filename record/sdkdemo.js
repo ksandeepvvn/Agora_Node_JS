c06b9547dfd6fe365c89b5e713bcb54a49f93367
Sandeep Kamarajugadda <ksandeepvvn@gmail.com>
2018-12-04 14:29:20 +0530

    App ID Added

--- record/sdkdemo.js
+++ record/sdkdemo.js
@@ -63,7 +63,7 @@ function startRecording() {
     //create output folder
     fs.mkdir(storageDir, {recursive: true}, err => {
         //join channel
-        rec.joinChannel(null, "agoratest", 0, YOUR_APP_ID, storageDir);
+        rec.joinChannel(null, "sandeep", 0, 4ea6c0ca6d9740f1a693b818ef905281, storageDir);
     })
     return rec;
 }
